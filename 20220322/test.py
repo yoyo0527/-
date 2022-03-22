d070738206f791a15f19c2423a035e59a7a74a37
yoyo0527 <C110156132@nkust.edu.tw>
2022-03-22 11:43:54 +0800

    0322

---
+++ 20220322/test.py
@@ -0,0 +1,12 @@
+a="c1101561"
+d=[]
+for i in range(1,57):
+    d.append(a+"{0:02d}".format(i))
+
+import random
+grade=[]
+for i in range(1,57):
+    grade.append(random.randint(0,100))
+
+b={"c110156132":60}
+print(b["c110156132"])
